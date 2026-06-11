3e5ebbdfe36889b37b76345b3cbf4a600bd93841
Joao Victor <belfort.joao216@gmail.com>
2026-06-11 17:57:18 -0300

    feat: developed Catalog Domain

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿namespace Catalog.Products.Models;
+
+public class Product : Aggregate<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public List<string> Category { get; private set; } = new();
+    public string Description { get; private set; } = default!;
+    public string ImageFile { get; private set; } = default!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(Guid id, string name, decimal price, string description, string imageFile,List<string> categoty)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Category = categoty,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+
+        product.AddDomainEvent(new ProductCreatedEvent(product));
+
+        return product;
+    }
+
+    public void Update(string name, decimal price, string description, string imageFile, List<string> categoty)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Name = name;
+        Category = categoty;
+        Price = price;
+        Description = description;
+        ImageFile = imageFile;
+
+        if(Price != price)
+        {
+            AddDomainEvent(new ProductPriceChangedEvent(this));
+        }
+    }
+}
