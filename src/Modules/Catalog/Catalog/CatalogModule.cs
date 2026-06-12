0b22d12d7cde45f5ad1e10a5a2ff4b2e4ec204f5
Joao Victor <belfort.joao216@gmail.com>
2026-06-12 18:06:50 -0300

    feat: catalog Migrations

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -8,11 +8,25 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+            
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddDbContext<CatalogDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
             return services;
         }
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            InitialiseDatabaseAsync(app).GetAwaiter().GetResult();
             return app;
+        }
+
+        private static async Task InitialiseDatabaseAsync(IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+            await context.Database.MigrateAsync();
         }
     }
 }
