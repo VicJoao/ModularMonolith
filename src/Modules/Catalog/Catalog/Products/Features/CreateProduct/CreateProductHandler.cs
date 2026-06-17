594f25c182883aad82637f32b1a79ba2fc8f424c
Joao Victor <belfort.joao216@gmail.com>
2026-06-16 23:30:46 -0300

    feat: CQRS mediatR abstractions

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand
+    (String Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
